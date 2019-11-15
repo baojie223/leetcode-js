093eb2d8fefc891637ecc18f71a4a5e9e0fd5d2a
baojie <baojie223@qq.com>
2019-11-15 16:04:18 +0800

    common push

---
+++ 206.反转链表.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  if (!head || !head.next) {
+    return head
+  } else {
+    const result = reverseList(head.next)
+    head.next.next = head
+    head.next = null
+    return result
+  }
+};
+// @lc code=end
+
---
+++ 509.斐波那契数.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=javascript
+ *
+ * [509] 斐波那契数
+ */
+
+// @lc code=start
+/**
+ * @param {number} N
+ * @return {number}
+ */
+var fib = function(N) {
+  if (N < 2) {
+    return N
+  } else {
+    return fib(N - 1) + fib(N - 2)
+  }
+};
+// @lc code=end
+
