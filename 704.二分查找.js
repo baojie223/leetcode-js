da03932a4fb6e9ff04bec347594118f618e2107a
baojie <baojie223@qq.com>
2019-12-03 17:24:16 +0800

    feat: add 2

---
+++ 704.二分查找.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=704 lang=javascript
+ *
+ * [704] 二分查找
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  let left = 0
+  let right = nums.length - 1
+  let mid
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2)
+    if (nums[mid] === target) {
+      return mid
+    }
+    if (nums[mid] < target) {
+      left = mid + 1
+    }
+    if (nums[mid] > target) {
+      right = mid - 1
+    }
+  }
+  return -1
+}
+// @lc code=end
