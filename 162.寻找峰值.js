93b0c0c2750ae555e6bbc14c66566e0193a2326e
baojie <baojie223@qq.com>
2019-12-06 10:22:03 +0800

    feat: 19/12/6 47-48

--- 162.寻找峰值.js
+++ 162.寻找峰值.js
@@ -13,21 +13,24 @@ var findPeakElement = function(nums) {
   let left = 0
   let right = nums.length - 1
   let mid
+  if (nums.length === 1) return 0
   while (left < right) {
     mid = Math.floor((left + right) / 2)
     if (nums[mid] < nums[mid + 1]) {
+      if (mid + 1 === nums.length - 1) return nums.length - 1
       if (nums[mid + 1] > nums[mid + 2]) {
         return mid + 1
       } else {
-        left = mid + 2
+        left = mid + 1
       }
       continue
     }
     if (nums[mid] > nums[mid + 1]) {
+      if (mid === 0) return 0
       if (nums[mid - 1] < nums[mid]) {
         return mid
       } else {
-        right = mid - 1
+        right = mid
       }
       continue
     }
