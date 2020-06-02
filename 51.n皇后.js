4f7dfa7089206a980fe0414fc2df2cd4eb9b17b5
baojie <baojie223@qq.com>
2020-06-02 16:53:51 +0800

    fix: 1

--- 51.n皇后.js
+++ 51.n皇后.js
@@ -21,33 +21,34 @@ function backtrack(board, row, res) {
     res.push([...board])
     return
   }
-  const n = board[row].length
-  for (let col = 0; col < n; col++) {
-    if (!isValid(board, row, col)) continue
-
-    board[row] = '.'.repeat(col) + 'Q' + '.'.repeat(n - col - 1)
-    backtrack(board, row + 1, res)
-    board[row] = '.'.repeat(n)
+  for (let col = 0; col < board.length; col++) {
+    if (isValid(board, row, col)) {
+      board[row] = '.'.repeat(col) + 'Q' + '.'.repeat(board.length - col - 1)
+      backtrack(board, row + 1, res)
+      board[row] = '.'.repeat(board.length)
+    }
   }
 }
 
 function isValid(board, row, col) {
-  const n = board.length
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < board.length; i++) {
     if (board[i][col] === 'Q') {
       return false
     }
   }
+
   for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
     if (board[i][j] === 'Q') {
       return false
     }
   }
-  for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+
+  for (let i = row - 1, j = col + 1; i >= 0 && j < board.length; i--, j++) {
     if (board[i][j] === 'Q') {
       return false
     }
   }
+
   return true
 }
 // @lc code=end
