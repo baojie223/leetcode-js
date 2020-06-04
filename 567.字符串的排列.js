324e77754dbac3a80666222421cfce741e972793
baojie <baojie223@qq.com>
2020-06-04 16:51:58 +0800

    refactor: 2

--- 567.字符串的排列.js
+++ 567.字符串的排列.js
@@ -11,42 +11,25 @@
  * @return {boolean}
  */
 var checkInclusion = function (s1, s2) {
-  const window = {}
-  const need = {}
+  const window = {},
+    need = {}
   for (let c of s1) {
     if (!need[c]) need[c] = 0
     need[c]++
   }
-
-  let left = 0
-  let right = 0
-  let valid = 0
-
+  let left = 0,
+    right = 0,
+    valid = 0
   while (right < s2.length) {
-    const c = s2[right]
-    right++
-    if (!window[c]) {
-      window[c] = 0
-    }
-    if (need[c]) {
-      window[c]++
-      if (window[c] <= need[c]) {
-        valid++
-      }
-    }
-
+    const c = s2[right++]
+    if (!window[c]) window[c] = 0
+    window[c]++
+    if (window[c] <= need[c]) valid++
     while (right - left >= s1.length) {
-      if (valid === s1.length) {
-        return true
-      }
-      const d = s2[left]
-      left++
-      if (need[d]) {
-        if (window[d] <= need[d]) {
-          valid--
-        }
-        window[d]--
-      }
+      const d = s2[left++]
+      if (valid === s1.length) return true
+      if (window[d] <= need[d]) valid--
+      window[d]--
     }
   }
   return false
--- 76.最小覆盖子串.js
+++ 76.最小覆盖子串.js
@@ -11,52 +11,34 @@
  * @return {string}
  */
 var minWindow = function (s, t) {
-  const window = {}
-  const need = {}
-
+  const window = {},
+    need = {}
   for (let c of t) {
     if (!need[c]) need[c] = 0
-
     need[c]++
   }
-
-  let left = 0
-  let right = 0
-  let valid = 0
-
-  let start = 0
-  let len = Number.MAX_SAFE_INTEGER
+  let left = 0,
+    right = 0,
+    valid = 0,
+    start = 0,
+    len = Number.MAX_SAFE_INTEGER
   while (right < s.length) {
-    const c = s[right]
-    if (!window[c]) {
-      window[c] = 0
-    }
-    right++
-
-    if (!!need[c]) {
+    const c = s[right++]
+    if (need[c]) {
+      if (!window[c]) window[c] = 0
       window[c]++
-      if (window[c] <= need[c] && window[c] !== 0) {
-        valid++
-      }
+      if (window[c] <= need[c]) valid++
     }
-
     while (valid === t.length) {
       if (right - left < len) {
         start = left
         len = right - left
       }
-      const d = s[left]
-      left++
-
-      if (!!need[d]) {
-        if (window[d] === need[d]) {
-          valid--
-        }
-        window[d]--
-      }
+      const d = s[left++]
+      if (window[d] <= need[d]) valid--
+      window[d]--
     }
   }
-
   return len === Number.MAX_SAFE_INTEGER ? '' : s.substr(start, len)
 }
 // @lc code=end
