37bce655b907fd66d6ffbdf57a9a715ba66c34a0
baojie <baojie223@qq.com>
2020-07-10 11:07:25 +0800

    打卡：每日一题

--- all/309.最佳买卖股票时机含冷冻期.js
+++ all/309.最佳买卖股票时机含冷冻期.js
@@ -10,16 +10,36 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-  const len = prices.length
-  let dp_i_0 = 0,
-    dp_i_1 = -Infinity
-  let dp_pre_0 = 0
-  for (let i = 0; i < len; i++) {
+  // 动态规划，dp[i][1] = Math.max(dp[i - 2][0] - prices[i], dp[i - 1][1]), dp[i][0] = Math.max(dp[i - 1][1] + prices[i], dp[i - 1][0])
+  // 起始状态：dp[0][1] = -prices[0], dp[0][0] = 0, dp[1][1] = Math.max(-prices[1], -prices[0]), dp[1][0] = Math.max(-prices[0] + prices[1], 0)
+
+  // if (!prices.length) return 0;
+  // const dp = [];
+  // for (let i = 0; i < prices.length; i++) {
+  //   if (!dp[i]) dp[i] = [];
+  //   if (i === 0) {
+  //     dp[0][1] = -prices[0];
+  //     dp[0][0] = 0;
+  //     continue;
+  //   }
+  //   if (i === 1) {
+  //     dp[1][1] = Math.max(-prices[1], -prices[0]);
+  //     dp[1][0] = Math.max(-prices[0] + prices[1], 0);
+  //     continue;
+  //   }
+  //   dp[i][1] = Math.max(dp[i - 2][0] - prices[i], dp[i - 1][1]);
+  //   dp[i][0] = Math.max(dp[i - 1][1] + prices[i], dp[i - 1][0]);
+  // }
+  // return dp[prices.length - 1][0];
+
+  // 优化空间复杂度，由数组变为常量
+  let dp_i_1 = -Infinity, dp_i_0 = 0, dp_pre_0 = 0
+  for (let i = 0; i < prices.length; i++) {
     const temp = dp_i_0
-    dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i])
-    dp_i_1 = Math.max(dp_i_1, dp_pre_0 - prices[i])
+    dp_i_0 = Math.max(dp_i_1 + prices[i], dp_i_0)
+    dp_i_1 = Math.max(dp_pre_0 - prices[i], dp_i_1)
     dp_pre_0 = temp
   }
   return dp_i_0
-}
+};
 // @lc code=end
