e62a55ef0bcea37a99bce5df25815ad5e6a40e21
baojie <baojie223@qq.com>
2020-07-14 17:59:26 +0800

    2020/7/14每日一题

--- all/120.三角形最小路径和.js
+++ all/120.三角形最小路径和.js
@@ -9,19 +9,31 @@
  * @param {number[][]} triangle
  * @return {number}
  */
-var minimumTotal = function(triangle) {
-  const dp = []
-  const len = triangle.length
-  dp[len - 1] = triangle[len - 1]
-  const min = (a, b) => {
-    return a >= b ? b : a
-  }
-  for (let i = len - 2; i >= 0; i--) {
-    dp[i] = []
-    for (let j = 0; j < triangle[i].length; j++) {
-      dp[i][j] = min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle[i][j]
+var minimumTotal = function (triangle) {
+  // 动态规划，状态转移方程：dp[i][j] = min(dp[i - 1][j- 1], dp[i - 1][j]) + t[i][j]
+  const rows = triangle.length;
+  if (!rows) return 0;
+  const dp = [];
+  for (let i = 0; i < rows; i++) {
+    if (!dp[i]) dp[i] = [];
+    if (i === 0) {
+      dp[0][0] = triangle[0][0];
+      continue;
+    }
+    for (let j = 0; j <= i; j++) {
+      if (j === 0) {
+        dp[i][0] = dp[i - 1][0] + triangle[i][0];
+        continue;
+      }
+      if (j === i) {
+        dp[i][i] = dp[i - 1][i - 1] + triangle[i][i];
+        continue;
+      }
+      dp[i][j] = Math.min(dp[i - 1][j - 1], dp[i - 1][j]) + triangle[i][j];
     }
   }
-  return dp[0][0]
-}
+
+  return Math.min(...dp[rows - 1]);
+};
 // @lc code=end
+minimumTotal([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]);
