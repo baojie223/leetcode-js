0f1a5a455786abacb355d730d55b1217bdce0a47
baojie <baojie223@qq.com>
2020-07-22 18:01:55 +0800

    2020/7/22打卡每日一题

--- 剑指offer/11.旋转数组的最小数字.js
+++ 剑指offer/11.旋转数组的最小数字.js
@@ -5,29 +5,16 @@
 var minArray = function (numbers) {
   let i = 0,
     j = numbers.length - 1,
-    mid = 0;
-
-  while (numbers[i] >= numbers[j]) {
-    if (j - i === 1) return numbers[j];
-    mid = Math.floor(i + (j - i) / 2);
-
-    if (numbers[mid] === numbers[i] && numbers[mid] === numbers[j]) {
-      return minArrayByOrder(numbers, i, j);
-    }
-
-    if (numbers[mid] >= numbers[i]) {
-      i = mid;
-    } else if (numbers[mid] <= numbers[j]) {
-      j = mid;
+    m = 0
+  while (i < j) {
+    m = Math.floor(i + (j - i) / 2)
+    if (numbers[m] < numbers[j]) {
+      j = m
+    } else if (numbers[m] > numbers[j]) {
+      i = m + 1
+    } else {
+      j--
     }
   }
-  return numbers[mid];
-};
-
-function minArrayByOrder(numbers, left, right) {
-  let min = Number.MAX_SAFE_INTEGER;
-  for (let i = left; i <= right; i++) {
-    min = Math.min(min, numbers[i]);
-  }
-  return min;
+  return numbers[i]
 }
