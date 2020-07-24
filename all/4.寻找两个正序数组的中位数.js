88c02f1ee07f5019c950d9f6995ec099e085fb64
jerry23 <baojiedashuaibi@gmail.com>
2020-07-24 17:49:40 +0800

    2020/7/24打卡每日一题

--- all/4.寻找两个正序数组的中位数.js
+++ all/4.寻找两个正序数组的中位数.js
@@ -10,8 +10,34 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findMedianSortedArrays = function(nums1, nums2) {
-  
-};
+var findMedianSortedArrays = function (nums1, nums2) {
+  // 找第k小的数字
+  const m = nums1.length,
+    n = nums2.length
+  const sortedArray = []
+  let i = 0,
+    j = 0,
+    k = Math.floor((m + n) / 2)
+  while (i + j <= k && i < m && j < n) {
+    if (nums1[i] < nums2[j]) {
+      sortedArray.push(nums1[i++])
+    } else {
+      sortedArray.push(nums2[j++])
+    }
+  }
+  while (i < m && i + j <= k) {
+    sortedArray.push(nums1[i++])
+  }
+  while (j < n && i + j <= k) {
+    sortedArray.push(nums2[j++])
+  }
+  if ((m + n) % 2 === 0) {
+    return (sortedArray[k - 1] + sortedArray[k]) / 2
+  } else {
+    return sortedArray[k]
+  }
+}
+
 // @lc code=end
 
+findMedianSortedArrays([], [1])
--- all/5.最长回文子串.js
+++ all/5.最长回文子串.js
@@ -9,39 +9,82 @@
  * @param {string} s
  * @return {string}
  */
-  var longestPalindrome = function(s) {
-    function judge(i, j) {
-      if (j < i + 3) {
-        return s[i] === s[j] ? true : false
-      }
-      if (judge(i + 1, j - 1) && s[i] === s[j]) {
-        return true
-      } else {
-        return false
-      }
-    }
-    if (s.length < 2) return s
-    const dp = []
-    let left = 0
-    let right = 0
-    for (let i = 0; i < s.length; i++) {
-      dp[i] = []
-      for (let j = i + 1; j < s.length; j++) {
-        dp[i][j] = judge(i, j)
-        if (dp[i][j]) {
-          if (j - i > right - left) {
-            right = j
-            left = i
-            // console.log(right, left)
-          }
-        }
-      }
-    }
-    // return dp
-    // console.log(dp)
-    // console.log(left, right)
-    return left === 0 && right === 0 ? s[0] : s.slice(left, right) + s[right]
-  }
-// @lc code=end
+// 暴力法, 时间O(n3), 空间O(1)
+// var longestPalindrome = function (s) {
+//   let maxLength = 0,
+//     left,
+//     right
+//   for (let i = 0; i < s.length; i++) {
+//     for (let j = i; j < s.length; j++) {
+//       if (j - i + 1 < maxLength) continue
+//       if (isPalindrome(s, i, j)) {
+//         maxLength = j - i + 1
+//         left = i
+//         right = j
+//       }
+//     }
+//   }
+//   return s.substr(left, maxLength)
+// }
 
-// console.log(longestPalindrome('aaaaaaaaaaaaaaaaaa'))
+// function isPalindrome(s, i, j) {
+//   while (i <= j) {
+//     if (s[i] !== s[j]) return false
+//     i++
+//     j--
+//   }
+//   return true
+// }
+
+// 动态规划, 时间O(n2), 空间O(n2)
+// var longestPalindrome = function (s) {
+//   const dp = Array(s.length)
+//     .fill('')
+//     .map(() => Array(s.length).fill(false))
+//   let begin = 0,
+//     maxLen = 1
+//   for (let i = 0; i < s.length; i++) {
+//     dp[i][i] = true
+//   }
+//   for (let j = 1; j < s.length; j++) {
+//     for (let i = 0; i < j; i++) {
+//       if (j - i === 1) {
+//         dp[i][j] = s[i] === s[j]
+//       } else {
+//         dp[i][j] = s[i] === s[j] && dp[i + 1][j - 1]
+//       }
+//       if (dp[i][j] && j - i + 1 > maxLen) {
+//         begin = i
+//         maxLen = j - i + 1
+//       }
+//     }
+//   }
+//   return s.substr(begin, maxLen)
+// }
+
+// 中心扩散法, 时间O(n2), 空间O(1)
+var longestPalindrome = function (s) {
+  let maxLenStr = s.substr(0, 1)
+  for (let i = 0; i < s.length - 1; i++) {
+    const odd = centerSpread(s, i, i)
+    const even = centerSpread(s, i, i + 1)
+    const str = odd.length > even.length ? odd : even
+    if (str.length > maxLenStr.length) {
+      maxLenStr = str
+    }
+  }
+  return maxLenStr
+}
+
+function centerSpread(s, i, j) {
+  while (i >= 0 && j < s.length) {
+    if (s[i] === s[j]) {
+      i--
+      j++
+    } else {
+      break
+    }
+  }
+  return s.substr(i + 1, j - i - 1)
+}
+// @lc code=end
