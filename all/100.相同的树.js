5be02d6600eb41d26788845b21ff558cef891df2
jerry23 <baojiedashuaibi@gmail.com>
2020-08-07 10:55:06 +0800

    2020/8/7打卡每日一题

--- all/100.相同的树.js
+++ all/100.相同的树.js
@@ -65,9 +65,9 @@
  * @return {boolean}
  */
 var isSameTree = function (p, q) {
+  if (p && !q) return false
+  if (!p && q) return false
   if (!p && !q) return true
-  if (!p || !q) return false
-  if (p.val === q.val) return isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
-  return false
+  return p.val === q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right)
 }
 // @lc code=end
--- all/31.下一个排列.js
+++ all/31.下一个排列.js
@@ -9,8 +9,62 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var nextPermutation = function(nums) {
+var nextPermutation = function (nums) {
+  const n = nums.length
+  if (n <= 1) return
+  let i = n - 1
+  while (i > 0 && nums[i] <= nums[i - 1]) i--
+  if (i === 0) {
+    sort(nums, 0, n - 1)
+    return
+  }
+  let min = findMin(nums, i - 1, n - 1)
+  exch(nums, i - 1, min)
+  sort(nums, i, n - 1)
+}
 
-};
+function sort(a, lo, hi) {
+  if (hi <= lo) return
+  const j = partition(a, lo, hi)
+  sort(a, lo, j - 1)
+  sort(a, j + 1, hi)
+}
+
+function partition(a, lo, hi) {
+  let i = lo,
+    j = hi + 1
+  const v = a[lo]
+  while (true) {
+    while (less(a[++i], v)) if (i === hi) break
+    while (less(v, a[--j])) if (j === lo) break
+    if (i >= j) break
+    exch(a, i, j)
+  }
+  exch(a, j, lo)
+  return j
+}
+
+function less(v, w) {
+  return v < w
+}
+
+function exch(a, i, j) {
+  const t = a[i]
+  a[i] = a[j]
+  a[j] = t
+}
+
+function findMin(nums, i, j) {
+  const target = nums[i++]
+  let min = i
+  while (i <= j) {
+    if (nums[i] < nums[min] && nums[i] > target) {
+      min = i
+    }
+    i++
+  }
+  return min
+}
 // @lc code=end
 
+nextPermutation([1, 3, 2])
