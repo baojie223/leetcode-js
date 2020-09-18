5b0ff327cb37a447f6ee9fb5e142813c02f65b7f
jerry23 <baojiedashuaibi@gmail.com>
2020-09-18 14:33:33 +0800

    2020/9/18打卡每日一题

--- all/47.全排列-ii.js
+++ all/47.全排列-ii.js
@@ -9,31 +9,32 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+// 回溯剪枝
 var permuteUnique = function (nums) {
-  const n = nums.length
+  nums.sort((a, b) => a - b)
   const path = [],
+    used = new Array(nums.length).fill(false),
     ans = []
-  dfs(path, nums, ans)
+  backtrack(nums, path, used, ans)
   return ans
+}
 
-  function dfs(path, nums, ans, bans = []) {
-    if (path.length === n) {
-      ans.push([...path])
-      return
-    }
-    const visited = []
-    for (let i = 0; i < nums.length; i++) {
-      if (bans.includes(i)) continue
-      if (visited.includes(nums[i])) continue
-      visited.push(nums[i])
-      path.push(nums[i])
-      bans.push(i)
-      dfs(path, nums, ans, bans)
-      bans.pop()
-      path.pop()
-    }
+function backtrack(nums, path, used, ans) {
+  if (path.length === nums.length) {
+    ans.push([...path])
+    return
+  }
+
+  for (let i = 0; i < nums.length; i++) {
+    if (used[i]) continue
+    if (i > 0 && nums[i] === nums[i - 1] && !used[i - 1]) continue
+    path.push(nums[i])
+    used[i] = true
+    backtrack(nums, path, used, ans)
+    used[i] = false
+    path.pop()
   }
 }
 
 // @lc code=end
-permuteUnique([1, 1, 2])
+permuteUnique([2, 2, 1, 1])
