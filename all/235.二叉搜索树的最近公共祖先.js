c18b4d23ac38eacbe4d6f3ba61ab80752d2b4074
jerry23 <baojiedashuaibi@gmail.com>
2020-09-28 11:09:27 +0800

    2020/9/28打卡每日一题

---
+++ all/235.二叉搜索树的最近公共祖先.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=235 lang=javascript
+ *
+ * [235] 二叉搜索树的最近公共祖先
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function (root, p, q) {
+  if (!root || (p.val <= root.val && q.val >= root.val) || (p.val >= root.val && q.val <= root.val)) return root
+  if (p.val < root.val && q.val < root.val) return lowestCommonAncestor(root.left, p, q)
+  if (p.val > root.val && q.val > root.val) return lowestCommonAncestor(root.right, p, q)
+}
+// @lc code=end
