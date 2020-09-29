2b37fe8a60a42740f6d744ee2a169af8774f7e72
jerry23 <baojiedashuaibi@gmail.com>
2020-09-29 19:40:47 +0800

    2020/9/29打卡每日一题

---
+++ all/145.二叉树的后序遍历.js
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=javascript
+ *
+ * [145] 二叉树的后序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root) {
+  // 递归
+  const ans = []
+  const postorder = (root) => {
+    if (!root) return
+    postorder(root.left)
+    postorder(root.right)
+    ans.push(root.val)
+  }
+
+  postorder(root)
+  return ans
+
+  // 
+  // const ans = []
+  // let pre = null, cur = root
+  // while (cur) {
+  //   let node = cur
+  //   while (node.left) {
+
+  //   }
+  // }
+}
+// @lc code=end
