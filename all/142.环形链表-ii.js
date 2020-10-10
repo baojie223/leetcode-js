8709790225a161d186f35b05d10957938e1aa0c4
jerry23 <baojiedashuaibi@gmail.com>
2020-10-10 10:07:40 +0800

    2020/10/10打卡每日一题

---
+++ all/142.环形链表-ii.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=javascript
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function (head) {
+  const set = new Set()
+  while (head) {
+    if (set.has(head)) return head
+    set.add(head)
+    head = head.next
+  }
+  return null
+}
+// @lc code=end
