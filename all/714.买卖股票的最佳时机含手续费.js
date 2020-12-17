27d0cbcc123e6a21562a398562dff66f89b43d31
jerry23 <baojiedashuaibi@gmail.com>
2020-12-17 11:20:10 +0800

    2020/12/17打卡每日一题

--- all/714.买卖股票的最佳时机含手续费.js
+++ all/714.买卖股票的最佳时机含手续费.js
@@ -10,14 +10,27 @@
  * @param {number} fee
  * @return {number}
  */
-var maxProfit = function(prices, fee) {
-  const n = prices.length
-  let dp_i_0 = 0, dp_i_1 = -Infinity
-  for (let i = 0; i < n; i++) {
-    dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i])
-    dp_i_1 = Math.max(dp_i_1, dp_i_0 - prices[i] - fee)
-  }
-  return dp_i_0
-};
-// @lc code=end
+// DP
+var maxProfit = function (prices, fee) {
+  // 保守做法, 空间O(n2)
+  // const n = prices.length
+  // const dp = [new Array(n), new Array(n)]
+  // dp[0][0] = 0
+  // dp[1][0] = -prices[0]
+  // for (let i = 1; i < n; i++) {
+  //   dp[0][i] = Math.max(dp[0][i - 1], dp[1][i - 1] + prices[i] - fee)
+  //   dp[1][i] = Math.max(dp[0][i - 1] - prices[i], dp[1][i - 1])
+  // }
+  // return Math.max(dp[0][n - 1], dp[1][n - 1])
 
+  // 降空间为O(1)
+  const n = prices.length
+  let dp0 = 0,
+    dp1 = -prices[0]
+  for (let i = 1; i < n; i++) {
+    dp0 = Math.max(dp0, dp1 + prices[i] - fee)
+    dp1 = Math.max(dp0 - prices[i], dp1)
+  }
+  return Math.max(dp0, dp1)
+}
+// @lc code=end
